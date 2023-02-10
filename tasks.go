ff6d7028c8cfa075417cd74aa8132dac895767ea
ov <oleksandr.volyanyk@gmail.com>
2023-02-10 16:29:12 +0100

    Tasks - getactive and add without tests

---
+++ tasks.go
@@ -0,0 +1,112 @@
+package golang_todoist_api
+
+import (
+	"context"
+	"encoding/json"
+	"net/url"
+	"strings"
+)
+
+type TasksResponse struct {
+	Tasks []Task
+	TodoistResponse
+}
+type TaskResponse struct {
+	Task Task
+	TodoistResponse
+}
+type Due struct {
+	Date        string `json:"date"`
+	IsRecurring bool   `json:"is_recurring"`
+	Datetime    string `json:"datetime"`
+	String      string `json:"string"`
+	Timezone    string `json:"timezone"`
+}
+
+type Task struct {
+	CreatorId    string   `json:"creator_id"`
+	CreatedAt    string   `json:"created_at"`
+	AssigneeId   string   `json:"assignee_id"`
+	AssignerId   string   `json:"assigner_id"`
+	CommentCount int      `json:"comment_count"`
+	IsCompleted  bool     `json:"is_completed"`
+	Content      string   `json:"content"`
+	Description  string   `json:"description"`
+	Due          Due      `json:"due"`
+	Id           string   `json:"id"`
+	Labels       []string `json:"labels"`
+	Order        int      `json:"order"`
+	Priority     int      `json:"priority"`
+	ProjectId    string   `json:"project_id"`
+	SectionId    string   `json:"section_id"`
+	ParentId     string   `json:"parent_id"`
+	Url          string   `json:"url"`
+}
+type AddTaskRequest struct {
+	Content     string   `json:"content"`
+	Description string   `json:"description"`
+	ProjectId   string   `json:"project_id"`
+	SectionId   string   `json:"section_id"`
+	ParentId    string   `json:"parent_id"`
+	Labels      []string `json:"labels"`
+	Order       int      `json:"order"`
+	Priority    int      `json:"priority"`
+	AssigneeId  string   `json:"assignee_id"`
+	DueString   string   `json:"due_string"`
+	DueDate     string   `json:"due_date"`
+	DueDatetime string   `json:"due_datetime"`
+	DueLang     string   `json:"due_lang"`
+}
+
+type GetActiveTasksRequest struct {
+	ProjectId string   `json:"project_id"` // Optional
+	SectionId string   `json:"section_id"` // Optional
+	Label     string   `json:"label"`      // Optional
+	Filter    string   `json:"filter"`     // Optional
+	Lang      string   `json:"lang"`       // Optional
+	Ids       []string `json:"ids"`        // Optional
+}
+
+func (api *Client) GetActiveTasks(getActiveTasksRequest GetActiveTasksRequest) (*[]Task, error) {
+	return api.GetActiveTasksContext(getActiveTasksRequest, context.Background())
+}
+func (api *Client) AddTask(request AddTaskRequest) (*Task, error) {
+	return api.AddTaskContext(request, context.Background())
+}
+
+func (api *Client) GetActiveTasksContext(request GetActiveTasksRequest, context context.Context) (*[]Task, error) {
+	response := &TasksResponse{}
+	values := url.Values{
+		"project_id": {request.ProjectId},
+		"section_id": {request.SectionId},
+		"label":      {request.Label},
+		"filter":     {request.Filter},
+		"lang":       {request.Lang},
+		"ids":        {strings.Join(request.Ids, ",")},
+	}
+
+	err := api.getMethod(context,
+		"tasks",
+		api.token,
+		values,
+		&response.Tasks)
+
+	return &response.Tasks, err
+}
+
+func (api *Client) AddTaskContext(addTaskRequest AddTaskRequest, context context.Context) (*Task, error) {
+	response := &TaskResponse{}
+
+	request, err := json.Marshal(addTaskRequest)
+
+	err = performPost(context, api.httpclient, api.endpoint+"tasks", api.token, request, &response, api)
+
+	if err != nil {
+		return nil, err
+	}
+	if !response.Ok {
+		return nil, response.Err()
+	}
+
+	return &response.Task, nil
+}
